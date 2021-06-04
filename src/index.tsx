dcadc651531908aa5fe579561ef01778ac052581
M Gilang Januar <mgilangjanuar@gmail.com>
2021-06-04 16:08:24 +0700

    use register-service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { register } from 'register-service-worker'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
-import { register } from './serviceWorker'
 import './index.css'
 
 ReactDOM.render(
@@ -15,5 +14,28 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
-register()
+// reportWebVitals()
+register('/service-worker.js', {
+  registrationOptions: { scope: './' },
+  ready(_registration) {
+    console.log('Service worker is active.')
+  },
+  registered(_registration) {
+    console.log('Service worker has been registered.')
+  },
+  cached(_registration) {
+    console.log('Content has been cached for offline use.')
+  },
+  updatefound(_registration) {
+    console.log('New content is downloading.')
+  },
+  updated(_registration) {
+    console.log('New content is available; please refresh.')
+  },
+  offline() {
+    console.log('No internet connection found. App is running in offline mode.')
+  },
+  error(error) {
+    console.error('Error during service worker registration:', error)
+  }
+})
