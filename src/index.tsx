f3eb0636a85ae46f6110d527952f34801932a2f8
M Gilang Januar <mgilangjanuar@gmail.com>
2021-06-04 17:02:27 +0700

    fix load service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { register } from 'register-service-worker'
 import App from './App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import reportWebVitals from './reportWebVitals'
 import './index.css'
 
 ReactDOM.render(
@@ -11,31 +12,12 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals()
-register('/service-worker.js', {
-  registrationOptions: { scope: './' },
-  ready(_registration) {
-    console.log('Service worker is active.')
-  },
-  registered(_registration) {
-    console.log('Service worker has been registered.')
-  },
-  cached(_registration) {
-    console.log('Content has been cached for offline use.')
-  },
-  updatefound(_registration) {
-    console.log('New content is downloading.')
-  },
-  updated(_registration) {
-    console.log('New content is available; please refresh.')
-  },
-  offline() {
-    console.log('No internet connection found. App is running in offline mode.')
-  },
-  error(error) {
-    console.error('Error during service worker registration:', error)
-  }
-})
+reportWebVitals()
