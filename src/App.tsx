9e78d084a72e05f9e06a4bf37af197be2a16a842
M Gilang Januar <mgilangjanuar@gmail.com>
2021-06-04 14:44:23 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { GithubOutlined, HomeOutlined, MenuFoldOutlined, MenuUnfoldOutlined, UserOutlined } from '@ant-design/icons'
+import { Divider, Layout, Menu, Typography } from 'antd'
+import React, { useState } from 'react'
+import Home from './pages/home'
 
-function App() {
+import 'antd/dist/antd.dark.min.css'
+
+
+function App(): React.ReactElement {
+  const [collapsed, setCollapsed] = useState<boolean>(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Layout>
+      <Layout.Sider onCollapse={col => setCollapsed(col)} breakpoint="lg" collapsedWidth="0" trigger={null} collapsible collapsed={collapsed} style={{ minHeight: '100vh' }}>
+        <Menu style={{ paddingTop: '60px' }} theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1" icon={<HomeOutlined />}>Home</Menu.Item>
+          <Menu.Item key="2" icon={<GithubOutlined />}>GitHub</Menu.Item>
+          <Menu.Item key="3" icon={<UserOutlined />}>Creator</Menu.Item>
+        </Menu>
+      </Layout.Sider>
+      <Layout>
+        <Layout.Header>
+          <span onClick={() => setCollapsed(!collapsed)}>{ collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined /> }</span>
+        </Layout.Header>
+        <Layout.Content style={{ padding: 24 }}>
+          <Home />
+        </Layout.Content>
+        <Layout.Footer style={{ textAlign: 'center' }}>
+          <Divider />
+          <Typography.Paragraph>
+            Repair JSON &copy; 2021
+          </Typography.Paragraph>
+          <Typography.Paragraph>
+            <a href="https://vercel.com?utm_source=restfire-studio&utm_campaign=oss">
+              <img src="https://www.datocms-assets.com/31049/1618983297-powered-by-vercel.svg" alt="Powered by Vercel" />
+            </a>
+          </Typography.Paragraph>
+        </Layout.Footer>
+      </Layout>
+    </Layout>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/home.tsx
@@ -0,0 +1,73 @@
+import { Button, Form, notification, Space, Switch } from 'antd'
+import { useForm } from 'antd/lib/form/Form'
+import TextArea from 'antd/lib/input/TextArea'
+import djson from 'dirty-json'
+import React, { useEffect, useState } from 'react'
+import ReactJson from 'react-json-view'
+
+
+const Home: React.FC = () => {
+  const example = {
+    object: { text: 'greeting :\n"halo"', from: '🤡 unknown', timestamp: 1406543845 },
+    string: '{"text":"greeting :\\n"halo"" "from": "🤡 unknown", timestamp: 1406543845'
+  }
+  const [form] = useForm()
+  const [object, setObject] = useState<any>(example.object)
+  const [prettyView, setPrettyView] = useState<boolean>(true)
+
+  useEffect(() => {
+    form.setFieldsValue({
+      json: example.string
+    })
+  }, [])
+
+  const onFixIt = async () => {
+    const { json } = form.getFieldsValue()
+    try {
+      const data = djson.parse(json)
+      if (typeof data !== 'object') {
+        throw new Error('Data is not an object')
+      }
+      notification.success({
+        message: 'Yeay!',
+        description: 'Scroll down to check your clean JSON'
+      })
+      setObject(data)
+    } catch (error) {
+      notification.error({
+        message: 'Oh, shit!',
+        description: error.message
+      })
+      setObject({})
+    }
+  }
+
+  const clear = async () => {
+    form.resetFields()
+    setObject({})
+  }
+
+  return (
+    <div>
+      <Form form={form} layout="vertical" onFinish={onFixIt}>
+        <Form.Item name="json" required rules={[{ required: true, message: 'This field cannot be blank' }]}>
+          <TextArea rows={10} />
+        </Form.Item>
+        <Form.Item style={{ textAlign: 'right' }}>
+          <Space>
+            <Button onClick={clear}>Clear</Button>
+            <Button type="primary" htmlType="submit">Fix it!</Button>
+          </Space>
+        </Form.Item>
+      </Form>
+      <Form.Item label="Pretty View &nbsp; &nbsp;">
+        <Switch onChange={checked => setPrettyView(checked)} checked={prettyView} />
+      </Form.Item>
+      <div>
+        {prettyView ? <ReactJson style={{ overflowX: 'auto' }} src={object} theme="bright" /> : <pre>{JSON.stringify(object, null, 2)}</pre>}
+      </div>
+    </div>
+  )
+}
+
+export default Home
